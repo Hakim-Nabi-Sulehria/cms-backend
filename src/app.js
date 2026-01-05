c943e8045467bafba403f1da94a4ee0715c3df02
Hakim <hakim.nabi@tmrconsult.com>
2026-01-05 10:38:14 +0500

    added url in cors , backend app.js

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ app.use(helmet());
 
 // CORS configuration
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
+  origin: ['http://localhost:3000', 'http://127.0.0.1:3000','https://cms-frontend-5mz5xemt8-hakimbt92-2453s-projects.vercel.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
